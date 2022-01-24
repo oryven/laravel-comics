6ff872309178c411209f9f7c2568a473e5cd5941
Orazio <orazioventrella@hotmail.it>
2022-01-24 17:53:16 +0100

    fix: layout pag comic

--- resources/views/comic.blade.php
+++ resources/views/comic.blade.php
@@ -3,8 +3,11 @@
 @section('content')
 
 <div class="container-comic">
-    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quibusdam nam possimus, dolorum odit culpa impedit iste. Est obcaecati aspernatur saepe provident dicta nam recusandae eum veniam deleniti ipsum? Eius, nihil.</p>
-
+    <h1>Batman</h1>
+    <div class="container">
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quibusdam nam possimus, dolorum odit culpa impedit iste. Est obcaecati aspernatur saepe provident dicta nam recusandae eum veniam deleniti ipsum? Eius, nihil.</p>
+        <img src="{{asset('storage/assets/img/superman.jpg')}}" alt="">
+    </div>
 </div>
 
 @endsection
