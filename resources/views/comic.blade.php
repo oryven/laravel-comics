11403c05ac1816d16626101a846bef1f80e705ed
Orazio <orazioventrella@hotmail.it>
2022-01-24 17:18:13 +0100

    feat: link pages with controllers

--- resources/views/comic.blade.php
+++ resources/views/comic.blade.php
@@ -1,7 +1,11 @@
 @extends('componentes.layouts.main-layout')
 
-@section('content-comic')
+@section('content')
 
-<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci repellat fugit, atque maiores eveniet nesciunt, sed quod omnis accusamus ipsam laborum? Sed, aperiam. Laudantium esse officiis pariatur. Consequatur, iusto odio.</p>
+<div class="container-comic">
+    
+
+</div>
 
 @endsection
+
